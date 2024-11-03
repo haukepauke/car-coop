9201c8a226dc0c755f0503c8379b25e5f81218e8
Hauke Altmann <hauke@haukepauke.de>
2024-11-03 18:56:51 +0100

    logout user to external website

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -36,9 +37,13 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    #[Route(
+        path: '/logout', 
+        name: 'app_logout'
+    )]
+    public function logout(Security $security): Response
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        $security->logout(false);
+        return $this->redirect('https://car-coop.net');
     }
 }
