587a156a0da283149706b634c488500f7013f6de
Hauke Altmann <hauke@haukepauke.de>
2026-03-12 06:43:34 +0100

    refactoring message handling

--- src/MessageHandler/Event/InformUserWhenTripAdded.php
+++ src/MessageHandler/Event/InformUserWhenTripAdded.php
@@ -6,18 +6,23 @@ use App\Message\Event\TripAddedEvent;
 use App\Repository\TripRepository;
 use App\Service\EventMailerService;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Mime\Address;
 
-class InformUserWhenTripAdded implements MessageHandlerInterface
+#[AsMessageHandler]
+class InformUserWhenTripAdded
 {
     private EventMailerService $mailer;
     private TripRepository $tr;
+    private string $mailerFromEmail;
+    private string $mailerFromName;
 
-    public function __construct(EventMailerService $mailer, TripRepository $tr)
+    public function __construct(EventMailerService $mailer, TripRepository $tr, string $mailerFromEmail, string $mailerFromName)
     {
         $this->mailer = $mailer;
         $this->tr = $tr;
+        $this->mailerFromEmail = $mailerFromEmail;
+        $this->mailerFromName = $mailerFromName;
     }
 
     public function __invoke(TripAddedEvent $event)
@@ -28,7 +33,7 @@ class InformUserWhenTripAdded implements MessageHandlerInterface
 
         $email = (new TemplatedEmail())
             ->subject('email.trip.added')
-            ->from(new Address('webmaster@car-coop.net', 'Car Coop Mail Bot'))
+            ->from(new Address($this->mailerFromEmail, $this->mailerFromName))
             ->htmlTemplate('event/email/trip.added.html.twig')
             ->context([
                 'trip' => $trip,
