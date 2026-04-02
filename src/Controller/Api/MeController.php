1e0d77f4749a9f9d93eea93a5ab460f2e24cda84
Hauke Altmann <hauke@haukepauke.de>
2026-04-02 13:07:35 +0200

    provide user data for API using apps

---
+++ src/Controller/Api/MeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Returns the currently authenticated user's profile.
+ */
+class MeController extends AbstractController
+{
+    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->json([
+            'id'                 => $user->getId(),
+            'email'              => $user->getEmail(),
+            'name'               => $user->getName(),
+            'color'              => $user->getColor(),
+            'profilePicturePath' => $user->getProfilePicturePath(),
+            'roles'              => $user->getRoles(),
+        ]);
+    }
+}
